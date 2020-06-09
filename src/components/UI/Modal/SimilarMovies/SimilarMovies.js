80d9881e7b968d73b6abb45d6ed5fad2a45c34eb
seid <seid.saydahmed@gmail.com>
2020-06-09 16:45:32 +0300

    similarMovies changed to brainhubeu/react-carousel

--- src/components/UI/Modal/SimilarMovies/SimilarMovies.js
+++ src/components/UI/Modal/SimilarMovies/SimilarMovies.js
@@ -6,6 +6,9 @@ import SimilarMovie from './SimilarMovie/SimilarMovie';
 
 import './SimilarMovies.css'
 
+import Carousel, { Dots } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
+
 const SimilarMovies = ({ selectedMediaData, selectedMediaType, history }) => {
 
   // Need in case URL is pasted
@@ -25,17 +28,38 @@ const SimilarMovies = ({ selectedMediaData, selectedMediaType, history }) => {
 
   //display similar media type
   let type = selectedMediaType === 'movie' ? 'movies' : 'tv series';
+
+  // Slick carousel settings
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1
+  };
   return (
     <div className='similarMedia'>
       <div className='similarMedia-wrapper'>
         <div className='similarMedia-title'>
           <p className="card-info">Similar {type}</p>
         </div>
-        <div className='similarMedia-content'>
+        <Carousel
+          slidesPerPage={10}
+          slidesPerScroll={1}
+          animationSpeed={1500}
+          autoPlay={3000}
+          stopAutoPlayOnHover
+          // offset={5}
+          itemWidth={115}
+          clickToChange
+          left
+          infinite
+          slides={similarMovie} />
+        {/* <div className='similarMedia-content'>
           <div className='inner-wrapper'>
             {similarMovie}
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   )
