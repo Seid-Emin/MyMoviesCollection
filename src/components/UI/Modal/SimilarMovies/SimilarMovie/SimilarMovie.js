afc5d978832ff641db8ba45b1ddb2b177a3de3fd
seid <seid.saydahmed@gmail.com>
2020-06-19 14:22:43 +0300

    on similar movies clicked correct routing

--- src/components/UI/Modal/SimilarMovies/SimilarMovie/SimilarMovie.js
+++ src/components/UI/Modal/SimilarMovies/SimilarMovie/SimilarMovie.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { withRouter } from "react-router";
 
 import './SimilarMovie.css';
 import noCoverImg from '../../../../../assets/images/no-cover.png';
@@ -9,9 +10,6 @@ import noCoverImg from '../../../../../assets/images/no-cover.png';
 // Object  configs
 import * as actions from '../../../../../store/actions';
 import TheMovieDB from '../../../../../configs/ApiMovies';
-
-
-
 
 
 class SimilarMovie extends Component {
@@ -26,13 +24,15 @@ class SimilarMovie extends Component {
 
   render() {
     const { result: { id, name, title, original_name, poster_path },
-      mediaType, filterType, currentPage } = this.props;
+      mediaType, filterType, currentPage, history, collections: { status } } = this.props;
 
 
     let posterPath = poster_path ? TheMovieDB.API_Img + poster_path : noCoverImg;
+
+    let linkPath = history.location.pathname.includes('collections') ? `/collections/${status}/id=${id}` : `/${mediaType}/${filterType}/page=${currentPage}/id=${id}`
     return (
       <div className="card-action" onClick={this.getSelectedVideo}>
-        <Link to={`/${mediaType}/${filterType}/page=${currentPage}/id=${id}`}>
+        <Link to={linkPath}>
           <img className='similarMovie-img' src={posterPath} alt={original_name} />
           <p className='similarMovie-title'>{title || name}</p>
         </Link>
@@ -44,7 +44,8 @@ class SimilarMovie extends Component {
 const mapStateToProps = state => {
   return {
     filterType: state.search.filterType,
-    currentPage: state.search.currentPage
+    currentPage: state.search.currentPage,
+    collections: state.collections
   }
 }
 
@@ -55,4 +56,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(SimilarMovie)
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(SimilarMovie))
